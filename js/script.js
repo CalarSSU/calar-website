674a8405de07692dbd8bcd2acb6d5919daf881fb
ArtemiyGranat <artemiigranat@gmail.com>
2022-11-25 20:56:44 +0400

    first version of website
    Co-authored-by: vikaromanova <romanowa.rom-viki@yandex.ru>

---
+++ js/script.js
@@ -0,0 +1,80 @@
+const body = document.querySelector('body');
+const progress = document.querySelector('.progress');
+document.addEventListener('scroll', function() {
+    let dectorHeight = document.documentElement.clientHeight;
+    let scrollHeight = window.pageYOffset;
+    let documentHeight = body.clientHeight;
+    let scrollProc = (scrollHeight + dectorHeight) * 100 / documentHeight;
+        if (scrollHeight >= dectorHeight) {
+            progress.classList.add('active');
+            progress.style.width = scrollProc + '%';
+        } else {
+            progress.classList.remove('active');
+        }
+        let dectorHeight2 = dectorHeight * 2;
+        if (scrollHeight >= dectorHeight2) {
+            progress.classList.add('orange');
+        } else {
+            progress.classList.remove('orange');
+        }
+        let dectorHeight3 = dectorHeight * 3;
+        if (scrollHeight >= dectorHeight3) {
+            progress.classList.add('green');
+        } else {
+            progress.classList.remove('green');
+        }
+})
+
+
+
+function init() {
+    let yourWrapperElement = document.getElementsByClassName("outer-wrapper")[0];
+    let whateverEaseFunctionYouWant = remaningScrollDistance => { return remaningScrollDistance / 15 + 1 };
+    uss.hrefSetup();
+
+    yourWrapperElement.addEventListener("wheel", event => {
+      
+      event.preventDefault();
+      event.stopPropagation();
+      uss.scrollXBy(event.deltaY, yourWrapperElement, null, false);             
+    }, {passive:false});
+
+    
+    uss.setXStepLengthCalculator(whateverEaseFunctionYouWant, yourWrapperElement);
+}
+
+$(function() {
+    $( ".btn_1" ).click(function() {
+      $( ".btn_1" ).addClass( "onclic", 250, validate());
+    });
+  
+    function validate() {
+      setTimeout(function() {
+        $( ".btn_1" ).removeClass( "onclic" );
+        $( ".btn_1" ).addClass( "validate", 450, callback() );
+      }, 2250 );
+    }
+      function callback() {
+        setTimeout(function() {
+          $( ".btn_1" ).removeClass( "validate" );
+        }, 1250 );
+      }
+    });
+
+    $(function() {
+        $( ".btn_2" ).click(function() {
+          $( ".btn_2" ).addClass( "onclic", 250, validate());
+        });
+      
+        function validate() {
+          setTimeout(function() {
+            $( ".btn_2" ).removeClass( "onclic" );
+            $( ".btn_2" ).addClass( "validate", 450, callback() );
+          }, 2250 );
+        }
+          function callback() {
+            setTimeout(function() {
+              $( ".btn_2" ).removeClass( "validate" );
+            }, 1250 );
+          }
+        });
